373a1bff5d4728b0c6f754d59663b3b260b92012
grahambell92 <graham.bell@monash.edu>
2019-12-08 07:47:12 +1100

    System command file new

---
+++ homeAssistant_local/startSystems.sh
@@ -0,0 +1,16 @@
+
+# Ensure scripts not started on any system
+# start home assistnat
+# start the mqtt server
+# start the rpi_zero satellite
+# start puller script to pull images from satellite
+
+# Ensure no tmux scripts/sessions are running
+tmux kill-server
+# start the main home assistant sever
+tmux new-session -d -s homeAssistant './startHomeAssistant.sh' &
+# start the mqtt publisher (local)
+tmux new-session -d -s mqtt 'python3 example_publisher.py' &
+
+## start the rpi_zero satellite
+#tmux new-session -d -s rpi_zero_satellite './start_rpiZeroSatellite_0.sh' &
---
+++ homeAssistant_local/start_rpiZeroSatellite_0.sh
@@ -0,0 +1,4 @@
+# tmux new-session -d -s webcam 'python3 runTimeLapse.py' &
+
+ssh pi@10.0.0.19
+./start_Tmux_rpiZero_webcamTimelapse.sh
