2c2807e3ba1119686d3e76bfdc21f8a47342fe7b
grahambell92 <graham.bell@monash.edu>
2019-12-08 08:07:56 +1100

    Updated structure

--- startSystems.sh
+++ startSystems.sh
@@ -7,10 +7,11 @@
 
 # Ensure no tmux scripts/sessions are running
 tmux kill-server
+chmod 775 ./homeAssistant_local/startHomeAssistant.sh
 # start the main home assistant sever
-tmux new-session -d -s homeAssistant './startHomeAssistant.sh' & #
+tmux new-session -d -s homeAssistant './homeAssistant_local/startHomeAssistant.sh' & #
 # start the mqtt publisher (local)
-tmux new-session -d -s mqtt 'python3 example_publisher.py' &
+tmux new-session -d -s mqtt 'python3 ./homeAssistant_local/example_publisher.py' &
 
 ## start the rpi_zero satellite
 #tmux new-session -d -s rpi_zero_satellite './start_rpiZeroSatellite_0.sh' &
