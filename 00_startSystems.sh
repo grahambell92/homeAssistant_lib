ba9a55a50c574a3073a331ad5490b4774e5bc29b
grahambell92 <graham.bell@monash.edu>
2019-12-08 22:38:15 +1100

    updated naming.

--- 00_startSystems.sh
+++ 00_startSystems.sh
@@ -9,9 +9,9 @@
 tmux kill-server
 chmod 775 ./homeAssistant_local/startHomeAssistant.sh
 # start the main home assistant sever
-tmux new-session -d -s homeAssistant './homeAssistant_local/startHomeAssistant.sh' & #
+tmux new-session -d -s homeAssistant './homeAssistant_local/00_startTmuxHomeAssistant.sh' & #
 # start the mqtt publisher (local)
-tmux new-session -d -s mqtt 'python3 ./homeAssistant_local/example_publisher.py' &
+tmux new-session -d -s mqtt 'python3 ./homeAssistant_local/03_example_publisher.py' &
 
 ## start the rpi_zero satellite
 #tmux new-session -d -s rpi_zero_satellite './start_rpiZeroSatellite_0.sh' &
