23b757e63cebcf0fc8f0a6173d8d9431bb6ef1b2
grahambell92 <graham.bell@monash.edu>
2019-12-24 09:17:56 +1100

    change dir

--- homeAssistant_local/00_startAll.sh
+++ homeAssistant_local/00_startAll.sh
@@ -1,17 +1,10 @@
 
-# Ensure scripts not started on any system
-# start home assistnat
-# start the mqtt server
-# start the rpi_zero satellite
-# start puller script to pull images from satellite
+chmod 775 ./00_startTmuxHomeAssistant.sh
+./00_startMe.sh
 
 # Ensure no tmux scripts/sessions are running
 tmux kill-server
-chmod 775 ./homeAssistant_local/00_startTmuxHomeAssistant.sh
-# start the main home assistant sever
-tmux new-session -d -s homeAssistant './00_startTmuxHomeAssistant.sh' & #
-# start the mqtt publisher (local)
-tmux new-session -d -s mqtt 'python3 ./03_example_publisher.py' &
 
+# AS1231333
 ## start the rpi_zero satellite
-tmux new-session -d -s rpi_zero_satellite './start_rpiZeroSatellite_0.sh' &
+#tmux new-session -d -s rpi_zero_satellite './start_rpiZeroSatellite_0.sh' &
