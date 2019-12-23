c79b05ccdccdf8e93a44f7388899c080536071cf
grahambell92 <graham.bell@monash.edu>
2019-12-23 23:27:49 +1100

    change script operational structure.

---
+++ homeAssistant_local/00_startMe.sh
@@ -0,0 +1,14 @@
+
+# Ensure scripts not started on any system
+# start home assistnat
+# start the mqtt server
+# start the rpi_zero satellite
+# start puller script to pull images from satellite
+
+# Ensure no tmux scripts/sessions are running
+tmux kill-server
+chmod 775 ./homeAssistant_local/00_startTmuxHomeAssistant.sh
+# start the main home assistant sever
+tmux new-session -d -s homeAssistant './00_startTmuxHomeAssistant.sh' & #
+# start the mqtt publisher (local)
+tmux new-session -d -s mqtt 'python3 ./03_example_publisher.py' &
