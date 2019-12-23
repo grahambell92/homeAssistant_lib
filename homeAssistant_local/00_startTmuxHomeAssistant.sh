ef5d48ac733ba08ed5ca7e3e9a08e866e305a464
grahambell92 <graham.bell@monash.edu>
2019-12-24 00:01:23 +1100

    change dir

--- homeAssistant_local/00_startTmuxHomeAssistant.sh
+++ homeAssistant_local/00_startTmuxHomeAssistant.sh
@@ -1,2 +1,2 @@
 # tmux new-session -d -s webcam 'python3 runTimeLapse.py' &
-tmux new-session -d -s homeAssistant './startHomeAssistant.sh'
+tmux new-session -d -s homeAssistant './01_startHomeAssistant.sh'
