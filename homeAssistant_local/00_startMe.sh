66c7d60271a6ca2414037a897daf33f0ca58c0d5
grahambell92 <graham.bell@monash.edu>
2019-12-24 01:00:58 +1100

    change dir

--- homeAssistant_local/00_startMe.sh
+++ homeAssistant_local/00_startMe.sh
@@ -11,7 +11,7 @@ chmod 775 ./00_startTmuxHomeAssistant.sh
 chmod 775 ./01_startHomeAssistant.sh
 echo hereeee
 # start the main home assistant sever
-tmux new-session -d -s homeAssistant './00_startTmuxHomeAssistant.sh' & #
+tmux new-session -d -s homeAssistant './00_startTmuxHomeAssistant.sh'
 # start the mqtt publisher (local)
-tmux new-session -d -s mqtt 'python3 ./03_example_publisher.py' &
+tmux new-session -d -s mqtt 'python3 ./03_example_publisher.py'
 # AS1231
