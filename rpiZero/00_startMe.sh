9d6d2e789005d1297747bb40ed46e1b01957e5c0
grahambell92 <graham.bell@monash.edu>
2019-12-28 13:50:54 +1100

    timer update

--- rpiZero/00_startMe.sh
+++ rpiZero/00_startMe.sh
@@ -1,6 +1,6 @@
 
+cd /home/pi/homeAssistant_lib/rpiZero/
 tmux kill-session
 #00_start_Tmux_rpiZero_webcamTimelapse.sh &
-cd /home/pi/homeAssistant_lib/rpiZero/
 tmux new-session -d -s webcam 'python3 01_run_rpiZero_webcamTimelapse.py'
 tmux new-session -d -s rpi_zero_mqtt 'python3 02_mqtt_alivePublish.py'
