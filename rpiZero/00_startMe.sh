1c074010f6bb0a3ac1a8eb5d897abddd5ff913c5
grahambell92 <graham.bell@monash.edu>
2020-01-01 12:57:30 +1100

    Get auto boot script to run all the way through.

--- rpiZero/00_startMe.sh
+++ rpiZero/00_startMe.sh
@@ -6,5 +6,5 @@ cd /home/pi/homeAssistant_lib/rpiZero/
 
 tmux kill-server
 
-tmux new-session -d -s rpi_zero_mqtt 'python3 02_mqtt_alivePublish.py' &
-tmux new-session -d -s webcam 'python3 01_run_rpiZero_webcamTimelapse.py'
+tmux new-session -d -s rpi_zero_mqtt 'python3 02_mqtt_alivePublish.py'
+# tmux new-session -d -s webcam 'python3 01_run_rpiZero_webcamTimelapse.py'
