e72a4602ba060b77cb93d98bfccddb09defae047
grahambell92 <graham.bell@monash.edu>
2020-01-01 14:12:55 +1100

    Get auto boot script to run all the way through.

--- homeAssistant_local/00_startAll.sh
+++ homeAssistant_local/00_startAll.sh
@@ -4,5 +4,6 @@ chmod 775 ./00_startMe.sh
 
 ## start the rpi_zero satellite
 
-ssh pi@10.0.0.20 ssh 'bash -s' < /home/pi/homeAssistant_lib/rpiZero/00_startMe.sh
+ssh pi@10.0.0.20 'bash -s' < /home/homeassistant/homeAssistant_lib/rpiZero/00_startMe.sh
+
 exit
