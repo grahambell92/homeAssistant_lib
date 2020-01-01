966b187afbd3dd5c6188deb1024bc022ed292104
grahambell92 <graham.bell@monash.edu>
2020-01-02 00:34:26 +1100

    Get auto boot script to run all the way through.

---
+++ homeAssistant_local/00_serialToMqttTest.py
@@ -0,0 +1,31 @@
+import time, string
+import serial
+import paho.mqtt.client as paho
+from readSerial2 import decode_float
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS
+)
+
+time.sleep(1)
+ser.flushInput();  # clean input buffer
+
+brokerIP = "10.0.0.19"
+client = paho.Client("client-002")
+client.connect(brokerIP)
+
+while True:
+    print('waiting...')
+    value = ser.readline()
+    num = decode_float(value[2:10])
+    print(num)
+    if type(num) == float:
+        client.publish("brightnessTopic3", num)
+        print('Mqtt Published ok.')
+
+client.disconnect() #disconnect
