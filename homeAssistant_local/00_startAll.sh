738d0547d7adc67bdd8c3809e6eb5995ea541bcd
grahambell92 <graham.bell@monash.edu>
2020-01-01 14:05:54 +1100

    Get auto boot script to run all the way through.

--- homeAssistant_local/00_startAll.sh
+++ homeAssistant_local/00_startAll.sh
@@ -4,6 +4,5 @@ chmod 775 ./00_startMe.sh
 
 ## start the rpi_zero satellite
 
-ssh pi@10.0.0.20
-/home/pi/homeAssistant_lib/rpiZero/00_startMe.sh
-#tmux new-session -d -s rpi_zero_satellite './start_rpiZeroSatellite_0.sh' &
+ssh pi@10.0.0.20 ssh 'bash -s' < /home/pi/homeAssistant_lib/rpiZero/00_startMe.sh
+exit
