cf0366ae6756a43d08ada479da9c0c0fcffb5103
grahambell92 <graham.bell@monash.edu>
2020-01-03 09:39:57 +1100

    Major update of webcam class.

--- py_lib/runWebcam_timelapse.py
+++ py_lib/runWebcam_timelapse.py
@@ -7,10 +7,11 @@ import os
 import shutil
 import time
 import imageio
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageChops
 import numpy as np
 import paho.mqtt.client as paho
 import glob
+import math
 
 class webcam_timelapse():
         def __init__(self, archiveBaseFolder='/home/pi/webcamImages/',):
@@ -69,6 +70,13 @@ class webcam_timelapse():
                 else:
                         print('No images for timelapse gif. Exiting.')
 
+        def image_entropy(self, img):
+                """calculate the entropy of an image"""
+                # this could be made more efficient using numpy
+                histogram = img.histogram()
+                histogram_length = sum(histogram)
+                samples_probability = [float(h) / histogram_length for h in histogram]
+                return -sum([p * math.log(p, 2) for p in samples_probability if p != 0])
 
         def motionCheck(self, currentImgPath, prevImgPath):
 
@@ -76,23 +84,26 @@ class webcam_timelapse():
                 currentImg = Image.open(currentImgPath)
                 prevImg = Image.open(prevImgPath)
 
-                currentImg_blur = currentImg.filter(ImageFilter.BLUR)
-                prevImg_blur = prevImg.filter(ImageFilter.BLUR)
+                imgDiff = ImageChops.difference(currentImg, prevImg)
+                imgEntropy = self.image_entropy(img=imgDiff)
 
-                buffer1 = np.asarray(currentImg_blur)
-                print('sum of img1', np.sum(buffer1))
-                buffer2 = np.asarray(prevImg_blur)
-                print('sum of img2', np.sum(buffer2))
-                # Subtract image2 from image1
+                if False:
+                        currentImg_blur = currentImg.filter(ImageFilter.BLUR)
+                        prevImg_blur = prevImg.filter(ImageFilter.BLUR)
 
-                buffer3 = buffer1 - buffer2
-                movementValue = np.sum(buffer3)/(buffer1.size)
+                        buffer1 = np.asarray(currentImg_blur)
+                        print('sum of img1', np.sum(buffer1))
+                        buffer2 = np.asarray(prevImg_blur)
+                        print('sum of img2', np.sum(buffer2))
+                        # Subtract image2 from image1
+
+                        buffer3 = buffer1 - buffer2
+                        movementValue = np.sum(buffer3)/(buffer1.size)
 
                 client = paho.Client("client-003")
                 brokerIP = "10.0.0.19"
                 client.connect(brokerIP)
-                now = datetime.datetime.now()
-                client.publish("rpiMotionTopic", float(movementValue))
+                client.publish("rpiMotionTopic", float(imgEntropy))
                 print('')
                 print('Motion value:', movementValue)
                 print('')
