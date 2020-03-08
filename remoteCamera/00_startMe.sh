a68dc361c0baa31459698b127ad93b263f9d5696
grahambell92 <graham.bell@monash.edu>
2020-03-09 00:42:05 +1100

    Updated gif compilation.

--- remoteCamera/00_startMe.sh
+++ remoteCamera/00_startMe.sh
@@ -8,4 +8,4 @@ tmux kill-server
 
 tmux new-session -d -s imgAcquirer 'python3 02_run_rpiZero_imgAcquirer.py' &
 tmux new-session -d -s rpi_zero_mqtt 'python3 03_mqtt_alivePublish.py' &
-tmux new-session -d -s gifBuilder 'python3 04_run_rpiZero_buildTimelapseGif.py' &
+# tmux new-session -d -s gifBuilder 'python3 04_run_rpiZero_buildTimelapseGif.py' &
