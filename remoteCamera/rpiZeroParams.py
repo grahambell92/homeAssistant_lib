a83bb869c8336d8c2808da2f12e6709582e6f272
grahambell92 <graham.bell@monash.edu>
2020-03-10 12:07:43 +1100

    Multi-camera setup

--- remoteCamera/rpiZeroParams.py
+++ remoteCamera/rpiZeroParams.py
@@ -13,7 +13,7 @@ remoteCam0_Settings = {
     'haLiveGifPath': haUser + '@' + haIP + ':' + haFolder + 'www/remoteCam0_timelapse.gif',
 
     # Rpi camera settings
-    'imgQuality': 6,
+    'imgQuality': 3,
     'flipVert': True,
     'flipHori': True,
 
@@ -44,7 +44,7 @@ remoteCam1_settings = {
     'haLiveGifPath': haUser + '@' + haIP + ':' + haFolder + 'www/remoteCam1_timelapse.gif',
 
     # Rpi camera settings
-    'imgQuality': 6,
+    'imgQuality': 3,
     'flipVert': True,
     'flipHori': True,
 
