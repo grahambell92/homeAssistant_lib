e3a58e0eb9f7a3b656b9cf2b1a5a7b17e8a907bc
grahambell92 <graham.bell@monash.edu>
2020-03-17 23:55:42 +1100

    Starting and maintaining connection work

--- remoteCamera/00_startMe.sh
+++ remoteCamera/00_startMe.sh
@@ -4,10 +4,12 @@ cd /home/pi/homeAssistant_lib/remoteCamera/
 # Disable the hdmi output to conserve power.
 /opt/vc/bin/tvservice -o
 
+python3 02_run_rpiZero_imgAcquirer.py &
+python3 03_mqtt_alivePublish.py &
 # Use the full path to start tmux
-/usr/bin/tmux set remain-on-exit on
-/usr/bin/tmux kill-server
+#/usr/bin/tmux set remain-on-exit on
+#/usr/bin/tmux kill-server
 
-/usr/bin/tmux new-session -d -s imgAcquirer 'python3 02_run_rpiZero_imgAcquirer.py' &
-/usr/bin/tmux new-session -d -s rpi_zero_mqtt 'python3 03_mqtt_alivePublish.py' &
+#/usr/bin/tmux new-session -d -s imgAcquirer 'python3 02_run_rpiZero_imgAcquirer.py' &
+#/usr/bin/tmux new-session -d -s rpi_zero_mqtt 'python3 03_mqtt_alivePublish.py' &
 # tmux new-session -d -s gifBuilder 'python3 04_run_rpiZero_buildTimelapseGif.py' &
