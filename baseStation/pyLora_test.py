389e9ec8cb84a68f1e689be3e54d47de91236ae0
grahambell92 <graham.bell@monash.edu>
2020-04-05 21:51:55 +1000

    getting Pylora to load.

--- baseStation/pyLora_test.py
+++ baseStation/pyLora_test.py
@@ -1,7 +1,16 @@
 from time import sleep
 
 import sys
-sys.path.append("./../../pySX127x/")
+import sys, os
+print(os.path.dirname(sys.path[0]))
+print(os.path.dirname(sys.path[1]))
+print(os.path.dirname(sys.path[2]))
+
+
+exit(0)
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'pySX127x'))
+# from mymodule import MyModule
+# sys.path.append("./../../pySX127x/")
 from pySX127x.LoRa import *
 from pySX127x.board_config import BOARD
 BOARD.setup()
