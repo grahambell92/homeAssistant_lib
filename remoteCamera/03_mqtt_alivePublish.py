4a935a3b8dd1f18e472c6ab8485308305f7dc7bd
grahambell92 <graham.bell@monash.edu>
2020-04-08 20:50:07 +1000

    Reporting only valid battery voltages.

--- remoteCamera/03_mqtt_alivePublish.py
+++ remoteCamera/03_mqtt_alivePublish.py
@@ -50,7 +50,7 @@ while True:
             # For the 12 v battery voltage divider with a 3.2 and a 9.9k ohm R2 and R1 combo.
             battVolt = 0.013052441*battRead + 0.004452565
             print('Battery voltage:', battVolt)
-            if ~np.isnan(battVolt):
+            if np.isfinite(battVolt):
                 client.publish(remoteCam_settings["mqttBattVoltPublishTopic"], '{:.3f}'.format(battVolt))
             else:
                 print('Bad battVolt:', battVolt)
