07eef751ffa6680d055426351832a53c34589f66
grahambell92 <graham.bell@monash.edu>
2020-04-11 00:46:21 +1000

    Getting the motion images gallery working

--- baseStation/06_updateMotionImages.py
+++ baseStation/06_updateMotionImages.py
@@ -3,6 +3,7 @@ import os
 import itertools
 import subprocess
 import time
+import shutil
 
 wwwFolder = '/home/pi/homeassistant/www/'
 avenueGallery = wwwFolder + 'avenueGallery/'
@@ -10,12 +11,17 @@ yardGallery = wwwFolder + 'yardGallery/'
 
 destinations = [avenueGallery, yardGallery]
 
+# Delete the galleries first.
+for destination in destinations:
+    shutil.rmtree(destination)
+
 currentImgFilenames = [
     'remoteCam0_currentImage_LQ', # .jpg
     'remoteCam1_currentImage_LQ', # '.jpg
 ]
 
-countCycler = itertools.cycle(itertools.count(10))
+
+countCycler = itertools.cycle(range(10))
 while True:
     for currentImgFilename, destination in zip(currentImgFilenames, destinations):
         os.makedirs(destination, exist_ok=True)
