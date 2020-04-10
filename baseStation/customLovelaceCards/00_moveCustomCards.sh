709bc57fc6314215c39bdee559e6afc8228a2bb9
grahambell92 <graham.bell@monash.edu>
2020-04-10 12:43:03 +1000

    Copy the feedparser custom component

--- baseStation/customLovelaceCards/00_moveCustomCards.sh
+++
@@ -1,5 +0,0 @@
-# Copy the configuration file to the home assistant location.
-cp logbook-card.js /home/pi/homeassistant/www/
-
-# Copy the list card to this folder
-cp list-card-master/list-card.js /home/pi/homeassistant/www/
--- baseStation/custom_components/00_copyCustomComponents.sh
+++
@@ -1,5 +0,0 @@
-# Check that the custom_components folder exists in the homeassistant config folder
-mkdir -p /home/pi/homeassistant/custom_components;
-# Copy the feedparser
-cd feedparser-master/custom_components
-cp -r feedparser /home/pi/homeassistant/custom_components/
