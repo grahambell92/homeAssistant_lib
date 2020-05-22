656bc961c18bcaca21fd14bfc63e41c0108430fe
grahambell92 <graham.bell@monash.edu>
2020-05-22 23:51:52 +1000

    Motion surveilance init

--- remoteCamera/testRelay.py
+++ remoteCamera/testRelay.py
@@ -3,7 +3,7 @@ from time import sleep  # Import the sleep function from the time module
 
 GPIO.setwarnings(False)  # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
-outPin = 11
+outPin = 13
 GPIO.setup(outPin, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
 while True:  # Run forever
     GPIO.output(outPin, GPIO.HIGH)  # Turn on
