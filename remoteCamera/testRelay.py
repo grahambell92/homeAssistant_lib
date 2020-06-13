70aec1c33c239ccd886c31f1c9984da6903fb51b
grahambell92 <graham.bell@monash.edu>
2020-06-14 00:00:14 +1000

    lampTesting

--- remoteCamera/testRelay.py
+++ remoteCamera/testRelay.py
@@ -6,7 +6,9 @@ GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 outPin = 13
 GPIO.setup(outPin, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
 while True:  # Run forever
+    print('Lamp is on...')
     GPIO.output(outPin, GPIO.HIGH)  # Turn on
     sleep(100)  # Sleep for 1 second
+    print('Lamp is off...')
     GPIO.output(outPin, GPIO.LOW)  # Turn off
     sleep(1)  # Sleep for 1 second
