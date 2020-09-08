981233297a9678aa6a6e56e64d0832e17090762e
grahambell92 <graham.bell@monash.edu>
2020-09-08 23:11:32 +1000

    updating to rsync for image storing.

--- remoteCamera/05_run_surveilanceCamera.py
+++ remoteCamera/05_run_surveilanceCamera.py
@@ -28,5 +28,5 @@ webcam.motionSurveilance(
     # quality=remoteCam_settings['imgQuality'],
     flipVert=remoteCam_settings['flipVert'],
     flipHorz=remoteCam_settings['flipHori'],
-    timelapseInterval=60,
+    timelapseInterval=3*60,
 )
