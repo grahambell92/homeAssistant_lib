369444bddf4b76cebf0dfc8ecee2d156f180b937
grahambell92 <graham.bell@monash.edu>
2021-01-03 21:06:04 +1100

    Enabling Motion detection for the camera stations.

--- py_lib/runWebcam_timelapse.py
+++ py_lib/runWebcam_timelapse.py
@@ -393,7 +393,7 @@ class webcam_timelapse():
                           flipHorz=False,
                           timelapseInterval=240,
                           removeOldData=False,
-                          cameraFPS=3,
+                          cameraFPS=0.8,
                           resolution=(3280, 2464),
                           delayBetweenMotionEvents=3.0,
                           minFramesToTrigMotion=3,
